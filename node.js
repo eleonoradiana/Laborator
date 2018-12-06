7d84fd87b94527874b79727e2c169012b63d5dc2
eleonoradiana <43841315+eleonoradiana@users.noreply.github.com>
2018-12-06 15:32:37 +0200

    Update node.js

--- node.js
+++ node.js
@@ -7,10 +7,3 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 app.use(express.static('public'))
 
-var fs = require('fs');
- 
-fs.readFile('DATA', 'utf8', function(err, contents) {
-    console.log(contents);
-});
- 
-console.log('after calling readFile');
